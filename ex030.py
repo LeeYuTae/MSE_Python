3085847c21c7d75dba383ad13801f719ff2b314f
LeeYuTae <75929177+LeeYuTae@users.noreply.github.com>
2020-12-13 20:07:59 +0900

    Update ex030.py

--- ex030.py
+++ ex030.py
@@ -5,7 +5,7 @@
 
 
 string = 'abcd'          # string : 문자열 
-string.replace('b','B')  # replace : 대체하다   string의 b를 B로 대체한다. -> replace ('a','b') a 를 b로 대체함. 하지만 문자열은 변경할 수 없음.
+string.replace('b','B')  # replace : 대체하다  replace ('a','b') a 를 b로 대체함. 하지만 문자열은 변경할 수 없음.
 print(string)
 #답 :abcd
 
